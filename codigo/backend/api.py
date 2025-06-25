87a25947dcb653845285341d67434ee8125e0c46
LiliaRosa19 <lilia.rosa@discente.ufma.br>
2025-06-25 11:48:03 -0300

    feat: implementação do backend do historico e ajustes no .gitignore

--- codigo/backend/api.py
+++ codigo/backend/api.py
@@ -1,20 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
-from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from database import db
+from auth import auth_bp
+from historico import historico_bp
 
 app = Flask(__name__)
 CORS(app)
 
-client = MongoClient(os.getenv("MONGO_URI"))
-db = client.scrpai_ia  # nome do banco
-
-# importa as rotas de autenticação
-from auth import auth_bp
 app.register_blueprint(auth_bp)
+app.register_blueprint(historico_bp)
 
 @app.route('/')
 def home():
