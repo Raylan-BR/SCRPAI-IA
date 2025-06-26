65a9efd3d5e951dbadd6df738abbde9014283fa3
Raylan <raylanbruno2002@gmail.com>
2025-06-25 23:33:30 -0300

    feat: adicionar rota /historico para servir página de histórico

--- codigo/backend/main.py
+++ codigo/backend/main.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.form_route import form_bp
 from routes.chatbot_route import chatbot_bp
+from routes.historico import historico_bp
 
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(chatbot_bp)
-app.register_blueprint(form_bp)
+app.register_blueprint(historico_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
