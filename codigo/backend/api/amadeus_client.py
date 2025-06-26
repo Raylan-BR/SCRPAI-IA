dfdc1e9831001781591db099803b45d110e5e266
Raylan <raylanbruno2002@gmail.com>
2025-06-26 00:05:18 -0300

    config: credenciais do amadeus

--- codigo/backend/api/amadeus_client.py
+++ codigo/backend/api/amadeus_client.py
@@ -5,8 +5,8 @@ import os
 load_dotenv()
 
 amadeus = Client(
-    client_id=os.getenv("Tnmlpf1ahFUOTvarigqiwbyArmF64c4j"),
-    client_secret=os.getenv("YfTgcGmfjOGJ8iqI")
+    client_id=os.getenv("AMADEUS_CLIENT_ID"),
+    client_secret=os.getenv("AMADEUS_CLIENT_SECRET")
 )
 
 def buscar_voos(origem, destino, data, adultos):
