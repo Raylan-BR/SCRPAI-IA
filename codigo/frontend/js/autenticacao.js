8c1b7fb4681093288e6741b75ff0ae40301beb4b
Raylan <raylanbruno2002@gmail.com>
2025-06-26 22:26:40 -0300

    style: editei a nav das telas

--- codigo/frontend/js/autenticacao.js
+++ codigo/frontend/js/autenticacao.js
@@ -6,7 +6,7 @@ document.getElementById('loginForm')?.addEventListener('submit', async function 
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch("http://localhost:5000/login", {
+        const response = await fetch("/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email, password })
